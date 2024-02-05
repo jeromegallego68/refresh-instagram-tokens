30d31b32ca091afe9b338c4a486141f1da7a28e6
Jerome Gallego <jeromegallego2@gmail.com>
2024-02-05 19:43:00 +0100

    update tokens

--- tokens.js
+++ tokens.js
@@ -1,11 +1,12 @@
-var TOKENS=[
-{
-"username":"abdullah.oguk",
-"token":"MYTOKEN"
-},
-
-
-
+var TOKENS = [
+	{
+		"username": "dr.grillz.bcn",
+		"token": "IGQWRPZAFhsUm0wbEFmNXpicThvUmpIblJKX3pFeWJ2NEtTdGhWaGNtYXZAsY1ZALOEEwRzVFVGFoZA2w2YnpLQmNBdDVGSnNORXRGMFRDMDJ2OEpyZA0UzMk51d25ueUEzT0JTYVYyVkhJbm1KanlLZADV6cDJKQ2FtOEkZD"
+	},
+	{
+		"username": "institut.bellarosa",
+		"token": "IGQWRNclpycTBvZAUZAZAajFUX1BhbmpIb1NuOVc3ZAFJSV3BiWEprb1VMNzZAZAcXdCNTJ0QXc0NzFZAZAS1iNGhMckVzeVdWNEJmT21iMVl1N1N5a0EwcUQyaVBlaFUxYUVLelBtbVg0Q3kwWUFBZAy1uc0xNNVZAPR29peVkZD"
+	},
 ]
 
 module.exports = { TOKENS }
